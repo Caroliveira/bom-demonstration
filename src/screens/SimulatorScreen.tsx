57eff6c368402e7b9f8ac740e387554869ee30bb
Carolina Oliveira <10094036640@estudante.ufjf.br>
2021-11-23 18:58:56 -0300

    Change simulator header

--- src/screens/SimulatorScreen.tsx
+++ src/screens/SimulatorScreen.tsx
@@ -1,11 +1,18 @@
 import { useTranslation } from 'react-i18next';
+import { FiArrowLeft } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
+import { IconButtonComponent } from '../components';
 
 const SimulatorScreen = (): JSX.Element => {
   const { t } = useTranslation();
+  const history = useHistory();
 
   return (
-    <div className="home">
-      <h1 className="home__title">404 - {t('pageNotFound')}</h1>
+    <div className="simulator">
+      <div className="simulator__header">
+        <IconButtonComponent Icon={FiArrowLeft} translationKey={t('back')} onClick={() => history.push('/diagram')} />
+        <h1 className="simulator__title">{t('simulator')}</h1>
+      </div>
     </div>
   );
 };
