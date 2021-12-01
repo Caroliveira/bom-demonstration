8fd12ce2d588ab80e5b70c3f81b76d3c162b4578
Carolina Oliveira <10094036640@estudante.ufjf.br>
2021-12-01 17:28:49 -0300

    Remove unused utils

--- src/utils/nodeUtils.ts
+++ src/utils/nodeUtils.ts
@@ -6,29 +6,6 @@ export const nodeById = (nodes: Node[], id?: string | null) => {
   return nodes.find((node) => node.id === id) as Node;
 };
 
-const compare = (subEl: Node, el: Node, attribute: keyof Node) => {
-  if (attribute === "data") return subEl.data.label === el.data.label;
-  return subEl[attribute] === el[attribute];
-};
-
-export const removeDuplicatedNodes = (
-  nodes: (Node | Node)[],
-  condition: (subEl: Node, el: Node) => boolean,
-  attribute: keyof Node
-) => {
-  return (nodes as Node[]).reduce((acc, el) => {
-    const index = acc.findIndex((subEl) => compare(subEl, el, attribute));
-    let addElement = true;
-
-    if (index > -1) {
-      if (condition(acc[index], el)) acc.splice(index, 1);
-      else addElement = false;
-    }
-
-    return addElement ? [...acc, el] : acc;
-  }, [] as Node[]);
-};
-
 export const nodeMounter = (name: string): Node => {
   return {
     id: uuid(),
